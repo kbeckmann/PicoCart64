66566e2d194324314c247030d29351048c1e4dda
Konrad Beckmann <konrad.beckmann@gmail.com>
2022-06-19 02:16:41 +0200

    PIO

--- sw/cic_test/cic_test.c
+++ sw/cic_test/cic_test.c
@@ -5,8 +5,14 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
+
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "cic_test.pio.h"
 
 #include "cic.h"
 #include "n64.h"
@@ -26,10 +32,29 @@ typedef enum {
     STATE_WAIT_WRITE_H,
 } cart_state_t;
 
-int cart(void)
+uint32_t ringbuf[10];
+uint32_t ringbuf_idx;
+
+void ringbuf_put(uint32_t value)
 {
-    
+    if (ringbuf_idx > sizeof(ringbuf) / 4) {
+        for (int i = 0; i < sizeof(ringbuf) / 4; i++) {
+            printf("%d: %08lX\n", i, ringbuf[i]);
+        }
+        ringbuf_idx = 0;
+    }
+
+    ringbuf[ringbuf_idx++] = value;
 }
+
+
+void cart_pin_forever(PIO pio, uint sm, uint offset) {
+    cart_program_init(pio, sm, offset);
+    pio_sm_set_enabled(pio, sm, true);
+
+    // pio->txf[sm] = (clock_get_hz(clk_sys) / (2 * freq)) - 3;
+}
+
 
 int main()
 {
@@ -41,12 +66,23 @@ int main()
         gpio_set_pulls(i, false, false);
     }
 
-    gpio_set_dir(PICO_LA1, GPIO_OUT);
-    gpio_put(PICO_LA1, 0);
-    gpio_set_dir(PICO_LA2, GPIO_OUT);
-    gpio_put(PICO_LA2, 0);
+    // gpio_set_dir(PICO_LA1, GPIO_OUT);
+    // gpio_put(PICO_LA1, 0);
+    // gpio_set_dir(PICO_LA2, GPIO_OUT);
+    // gpio_put(PICO_LA2, 0);
+
+
 
     cic_run();
+
+    // N64_COLD_RESET = 1, n64 booted
+
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &cart_program);
+    cart_pin_forever(pio, 0, offset);
+
+    // pio_sm_put_blocking(pio, 0, 0x00000000); // AD0 -> AD15 = IN
+
 
     // multicore_launch_core1(cic_run);
 
@@ -56,66 +92,84 @@ int main()
     uint32_t n64_addr_h = 0;
     uint32_t n64_addr_l = 0;
 
+    uint8_t tmp[256];
+
     while (1) {
+
+        uint32_t data = pio_sm_get_blocking(pio, 0);
+        // pio_sm_put_blocking(pio, 0, 0xFFFF); // Data MSB
+        // pio_sm_put_blocking(pio, 0, 0xFFFF); // Data LSB
+
+        pio_sm_put_blocking(pio, 0, 0x8037); // Data MSB
+        pio_sm_put_blocking(pio, 0, 0x1240); // Data LSB
+
+
+        // ringbuf_put(data);
+
+
+
+
+
+
         // cic_process();
 
-        switch (state) {
+        // switch (state) {
 
-        case STATE_INIT:
-            if (gpio_get(N64_COLD_RESET)) {
-                state = STATE_START;
-                // printf("State => %d\n", state);
-            }
-            break;
+        // case STATE_INIT:
+        //     if (gpio_get(N64_COLD_RESET)) {
+        //         state = STATE_START;
+        //         // printf("State => %d\n", state);
+        //     }
+        //     break;
 
-        case STATE_START:
-            if (gpio_get(N64_ALEL) && gpio_get(N64_ALEH)) {
-                state = STATE_WAIT_ADDR_H;
-                gpio_put(PICO_LA1, 0);
-                gpio_put(PICO_LA2, 1);
+        // case STATE_START:
+        //     if (gpio_get(N64_ALEL) && gpio_get(N64_ALEH)) {
+        //         state = STATE_WAIT_ADDR_H;
+        //         // gpio_put(PICO_LA1, 0);
+        //         // gpio_put(PICO_LA2, 1);
 
-                // printf("State => %d\n", state);
-            }
-            if (!gpio_get(N64_COLD_RESET))
-                state = STATE_INIT;
-            break;
+        //         // printf("State => %d\n", state);
+        //     }
+        //     if (!gpio_get(N64_COLD_RESET))
+        //         state = STATE_INIT;
+        //     break;
 
-        case STATE_WAIT_ADDR_H:
-            if (gpio_get(N64_ALEL) && !gpio_get(N64_ALEH)) {
-                gpio_put(PICO_LA1, 1);
-                gpio_put(PICO_LA2, 0);
+        // case STATE_WAIT_ADDR_H:
+        //     if (gpio_get(N64_ALEL) && !gpio_get(N64_ALEH)) {
+        //         // gpio_put(PICO_LA1, 1);
+        //         // gpio_put(PICO_LA2, 0);
 
-                // Store 16 bits from ADxx
-                uint32_t all = gpio_get_all();
-                // printf("n64_addr_h: %08lx\n", all);
-                n64_addr_h = all & 0xFFFF;
-                state = STATE_WAIT_ADDR_L;
-                // printf("State => %d\n", state);
-            }
-            if (!gpio_get(N64_COLD_RESET))
-                state = STATE_INIT;
-            break;
+        //         // Store 16 bits from ADxx
+        //         uint32_t all = gpio_get_all();
+        //         // printf("n64_addr_h: %08lx\n", all);
+        //         n64_addr_h = all & 0xFFFF;
+        //         state = STATE_WAIT_ADDR_L;
+        //         // printf("State => %d\n", state);
+        //     }
+        //     if (!gpio_get(N64_COLD_RESET))
+        //         state = STATE_INIT;
+        //     break;
 
-        case STATE_WAIT_ADDR_L:
-            if (!gpio_get(N64_ALEL) && !gpio_get(N64_ALEH)) {
-                gpio_put(PICO_LA1, 1);
-                gpio_put(PICO_LA2, 1);
+        // case STATE_WAIT_ADDR_L:
+        //     if (!gpio_get(N64_ALEL) && !gpio_get(N64_ALEH)) {
+        //         // gpio_put(PICO_LA1, 1);
+        //         // gpio_put(PICO_LA2, 1);
 
-                uint32_t all = gpio_get_all();
-                // printf("n64_addr_l: %08lx\n", all);
-                n64_addr_l = all & 0xFFFF;
-                n64_addr = n64_addr_l | (n64_addr_h << 16);
+        //         uint32_t all = gpio_get_all();
+        //         // printf("n64_addr_l: %08lx\n", all);
+        //         n64_addr_l = all & 0xFFFF;
+        //         n64_addr = n64_addr_l | (n64_addr_h << 16);
 
-                // printf("Addr: %08lx\n", n64_addr);
+        //         // printf("Addr: %08lx\n", n64_addr);
 
-                state = STATE_WAIT_READ_WRITE;
-                // printf("State => %d\n", state);
-            }
-            if (!gpio_get(N64_COLD_RESET))
-                state = STATE_INIT;
-            break;
+        //         state = STATE_WAIT_READ_WRITE;
+        //         // printf("State => %d\n", state);
+        //     }
+        //     if (!gpio_get(N64_COLD_RESET))
+        //         state = STATE_INIT;
+        //     break;
 
-        }
+        // }
     }
     return 0;
 }
