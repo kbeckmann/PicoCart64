d3090c9fcba34319d7153c67a2d328909bbba1bd
Konrad Beckmann <konrad.beckmann@gmail.com>
2022-07-02 08:19:33 +0200

    sw: Add script to load a .z64 rom file into rom.h

---
+++ sw/scripts/load_rom.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Load .z64 rom into rom.h')
+    parser.add_argument('rom', type=str)
+
+    args = parser.parse_args()
+
+    rom_h = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'cic_test', 'rom.h')
+
+    with open(args.rom, 'rb') as r:
+        with open(rom_h, 'w') as f:
+            f.write('const unsigned char __in_flash("rom_file") rom_file[] = {\n')
+            f.write(','.join([hex(c) for c in r.read()]))
+            f.write('\n};\n')
+
+    print(f'Wrote rom contents to {rom_h}')
