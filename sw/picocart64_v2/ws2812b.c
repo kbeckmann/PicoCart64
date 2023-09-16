90b67173802c75780d275d69a4eb24951c2714dd
GitHub CI <kbeckmann@users.noreply.github.com>
2023-09-16 17:28:49 +0000

    Automated changes

--- sw/picocart64_v2/ws2812b.c
+++ sw/picocart64_v2/ws2812b.c
@@ -2,7 +2,7 @@
 
 #include "ws2812b.h"
 
-void ws2812b_write(int gpio, const uint8_t *pixels, uint32_t num_bytes)
+void ws2812b_write(int gpio, const uint8_t * pixels, uint32_t num_bytes)
 {
 	// Either use PIO based on https://github.com/adafruit/circuitpython/blob/main/ports/raspberrypi/common-hal/neopixel_write/__init__.c
 	// or bitbang?
